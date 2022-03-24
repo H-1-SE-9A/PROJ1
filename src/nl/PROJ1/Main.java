06ef6d74c593d13e240fd49744829da36339cb49
Ardemium <ArthurDjamardzhashvili@outlook.com>
2022-03-24 14:03:14 +0100

    jan branche

--- src/nl/PROJ1/Main.java
+++ src/nl/PROJ1/Main.java
@@ -1,13 +1,20 @@
 package nl.PROJ1;
 
+import nl.PROJ1.Objects.Examen;
 import nl.PROJ1.Objects.User;
 import nl.PROJ1.screens.ExamenAfnemen;
 
 public class Main {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
 <<<<<<< HEAD
         //Controller.start();
         ASON.changeValue("Gebruikersnummer", "19043058", "Examen3", "OPT8", "UserInformation");
+=======
+        Examen examen = new Examen("ExamenScrum");
+
+        System.out.println(examen.getFout(1,2));
+>>>>>>> Stashed changes
 
 =======
         ExamenAfnemen.start();
