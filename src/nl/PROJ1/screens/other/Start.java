bd11d2c24c49a5016c94fe9d939667d9356c4a7b
Karam <krm-011@hotmail.com>
2022-04-04 17:13:28 +0200

    Start.java gefixt met error check

--- src/nl/PROJ1/screens/other/Start.java
+++ src/nl/PROJ1/screens/other/Start.java
@@ -3,26 +3,43 @@ package nl.PROJ1.screens.other;
 import java.util.Scanner;
 
 public class Start {
+    static int choice = 99999;
 
     public static int startScreen() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Maak hieronder een keuze");
-        System.out.println("1- Inloggen");
-        System.out.println("2- Registreren");
-        System.out.print("Type hier je keuze in of type 0 voor Exit: ");
-        int choice = scanner.nextInt();
-        switch (choice) {
-            case 0 -> {
-                choice = 0;
-            }
-            case 1 -> {
-                choice = 1;
-            }
-            case 2 -> {
-                choice = 8;
+
+        boolean goodchoice = false;
+        while (!goodchoice) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Maak hieronder een keuze");
+            System.out.println("1- Inloggen");
+            System.out.println("2- Registreren");
+            System.out.print("Type hier je keuze in of type 0 voor Exit: ");
+
+
+            if (scanner.hasNextInt()) {
+                choice = Integer.parseInt(scanner.next());
+
+                switch (choice) {
+                    case 0 -> {
+                        goodchoice = true;
+                        choice = 0;
+                    }
+                    case 1 -> {
+                        goodchoice = true;
+                        choice = 1;
+                    }
+                    case 2 -> {
+                        goodchoice = true;
+                        choice = 8;
+                    }
+
+                }
+            } else {
+                System.out.println("ongeldige keuze");
+
             }
 
         }
-        return choice;
-    }
-}
+            return choice;
+
+    }}
